dca2c4ca3e5bfac556aad67cf15bb3191e712890
Farseena-M <farseenamoyan19@gmail.com>
2023-10-11 10:08:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+/* import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+       
+        const first10Quotes = data.slice(0, 10);
+        setQuotes(first10Quotes);
+      })
+      .catch((error) => {
+        console.error('Error fetching quotes:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Top 10 Quotes</h1>
+      <ul>
+        {quotes.map((quote, index) => (
+          <li key={index}>
+            <h2>{quote.title}</h2>
+            <p>{quote.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App; */
+
+
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [posts,setPosts]=useState('')
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response)=>response.json())
+    .then((posts)=>setPosts(posts.splice(0,10)))
+  },[])
+
+  return (
+    <div>
+         {posts.map((post)=><h5>{post.title} <br></br>{post.body}</h5>)}
+
+    </div>
+  )
+}
+
+export default App
